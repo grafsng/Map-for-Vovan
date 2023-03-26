28e5d9c7e6da4a4dfc03cbf646a7a734c106396d
grafsng <85547693+grafsng@users.noreply.github.com>
2023-03-26 23:27:08 +0300

    ddd
    dd

---
+++ data/_4.js
@@ -0,0 +1,10 @@
+var json__4 = {
+"type": "FeatureCollection",
+"name": "_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "2", "Фото": null, "name": "Park Inn by Radisson Novokuznetsk", "Адрес:": null, "Режим работы:": null, "Минимальная стоимост размещения:": null, "Сайт:": null, "Контакты:": null }, "geometry": { "type": "Point", "coordinates": [ 87.140409615522259, 53.763722164062159 ] } },
+{ "type": "Feature", "properties": { "fid": "3", "Фото": null, "name": "Замок Эдельвейс", "Адрес:": null, "Режим работы:": null, "Минимальная стоимост размещения:": null, "Сайт:": null, "Контакты:": null }, "geometry": { "type": "Point", "coordinates": [ 87.96272065368764, 52.767188061097904 ] } },
+{ "type": "Feature", "properties": { "fid": "4", "Фото": "<a href=\"https://azimuthotels.com/ru/kemerovo\"><img src=\"Images/Azimut.jpg\" width='250' height='150'>", "name": "Отель Азимут", "Адрес:": "пр. Ленина, 90/2, Кемерово", "Режим работы:": "Круглосуточно", "Минимальная стоимост размещения:": 4500.0, "Сайт:": null, "Контакты:": "+7 (3842) 49-65-05" }, "geometry": { "type": "Point", "coordinates": [ 86.12976373771059, 55.34580727463802 ] } }
+]
+}
